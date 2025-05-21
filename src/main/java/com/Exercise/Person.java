197ae31dba8c96628f3b2cce9e0b59432ad556a5
Joshua Vickers <JVZish@gmail.com>
2025-05-21 11:41:14 -0500

    person & person main added

--- src/main/java/com/Exercise/Person.java
+++ src/main/java/com/Exercise/Person.java
@@ -1,4 +1,37 @@
 package com.Exercise;
 
-public class Person {
+public class Person implements Comparable<Person> {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public int getAge() { return age; }
+
+    @Override
+    public int compareTo(Person other) {
+        int lastNameCompare = this.lastName.compareTo(other.lastName);
+        if (lastNameCompare != 0) {
+            return lastNameCompare;
+        }
+
+        int firstNameCompare = this.firstName.compareTo(other.firstName);
+        if (firstNameCompare != 0) {
+            return firstNameCompare;
+        }
+
+        return Integer.compare(this.age, other.age); // If first and last names are same, compare age
+    }
+    
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + ", Age: " + age;
+    }
 }
--- src/main/java/com/pluralsight/PortfolioApp.java
+++ src/main/java/com/pluralsight/PortfolioApp.java
@@ -1,8 +1,48 @@
 package com.pluralsight;
 
+import Finance.BankAccount;
 import Finance.CreditCard;
+
+import java.util.Scanner;
 
 public class PortfolioApp {
 
     CreditCard cc = new CreditCard();
+
+    public static void main(String[] args) {
+        Portfolio portfolio = new Portfolio();
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        do {
+            System.out.print("Enter asset type (house, bank, credit): ");
+            String type = scanner.nextLine().toLowerCase();
+
+            System.out.print("Enter asset value: ");
+            double value = Double.parseDouble(scanner.nextLine());
+
+            switch (type) {
+                case "house":
+                    portfolio.addAsset(new House(value));
+                    break;
+                case "bank":
+                    portfolio.addAsset(new BankAccount(value));
+                    break;
+                case "credit":
+                    portfolio.addAsset(new CreditCard(value));
+                    break;
+                default:
+                    System.out.println("Unknown type.");
+            }
+
+            System.out.print("Add another asset? (yes/no): ");
+            input = scanner.nextLine();
+        } while (input.equalsIgnoreCase("yes"));
+
+        scanner.close();
+
+        System.out.println("Total portfolio value: $" + portfolio.getValue());
+        System.out.println("Most valuable asset: $" + portfolio.getMostValuable().getValue());
+        System.out.println("Least valuable asset: $" + portfolio.getLeastValuable().getValue());
+    }
 }
